c7978eee788b87ad20722e22f1e21277eda46453
Lucas Saliba <lucassaliba10@gmail.com>
2022-01-16 19:09:28 -0300

    add submit on input

--- seven-segment-display/src/components/Input/Input.jsx
+++ seven-segment-display/src/components/Input/Input.jsx
@@ -2,12 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 import * as S from "./InputStyle";
 
-const Input = ({ placeholder }) => {
-  return <S.Input placeholder={placeholder} />;
+const Input = ({ name, type, placeholder, value, onChange }) => {
+  return (
+    <S.Input
+      name={name}
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
+  );
 };
 
 Input.propTypes = {
+  name: PropTypes.string,
+  type: PropTypes.string,
   placeholder: PropTypes.string,
+  value: PropTypes.string,
 };
 
 export default Input;
--- seven-segment-display/src/containers/Home/Home.jsx
+++ seven-segment-display/src/containers/Home/Home.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+// import { useDispatch } from "react-redux";
 import * as S from "./HomeStyle";
 import Title from "../../components/Title/Title";
 import Input from "../../components/Input/Input";
@@ -7,6 +8,19 @@ import Mensage from "../../components/Mensage/Mensage";
 import { ReactComponent as IconRefresh } from "../../assets/icons/icon_refresh.svg";
 
 const Home = () => {
+  // const dispatch = useDispatch();
+  const [guess, setGuess] = useState("");
+
+  const handleGuess = async (event) => {
+    event.preventDefault();
+
+    console.log(guess);
+    setGuess("");
+    // dispatch(
+    //   AuthActions.userAuthRequest({ email, password, recaptchaSuccess })
+    // );
+  };
+
   return (
     <S.Container>
       <Title>QUAL É O NÚMERO?</Title>
@@ -18,9 +32,15 @@ const Home = () => {
           NOVA PARTIDA
         </Button>
       </S.ContainerNumber>
-      <S.FormContainer>
-        <Input placeholder={"Digite o palpite"} />
-        <Button>ENVIAR</Button>
+      <S.FormContainer onSubmit={(event) => handleGuess(event)}>
+        <Input
+          type="text"
+          name="guess"
+          placeholder={"Digite o palpite"}
+          onChange={(e) => setGuess(e.target.value)}
+          value={guess}
+        />
+        <Button type="submit">ENVIAR</Button>
       </S.FormContainer>
     </S.Container>
   );
